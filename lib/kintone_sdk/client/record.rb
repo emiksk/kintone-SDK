574ebced2c0d1f7bc9799c2ae00cd3bd971e6ec1
Keisuke Emi <emiksk0910@gmail.com>
2018-05-31 09:27:49 +0900

    Implment update method for record

--- lib/kintone_sdk/client/record.rb
+++ lib/kintone_sdk/client/record.rb
@@ -19,15 +19,15 @@ module KintoneSDK
       end
 
       def update(app_id, payload, options = {})
-        body = convert_for_kintone(app_id, payload)
-        response = @client.put(@url, fmt)
+        body = convert_for_kintone(app_id, payload, options)
+        response = @client.put(@url, body)
         # KintoneSDK::Resource::Record.new(@client, response.body)
       end
 
       def delete(app_id, record_id)
-        # oh my kintone API !!!!!!!!!!
+        # Oh My kintone API !!!!!!!!!!
         url = @url.gsub(/record.json$/, "records.json")
-        p url
+
         @client.delete(url, app: app_id, ids: [record_id])
       end
 
@@ -39,13 +39,40 @@ module KintoneSDK
       end
 
       # to be continue...
-      def convert_for_kintone(app_id, hash)
-        body = { app: app_id }
-        if hash[:id]
+      def convert_for_kintone(app_id, hash, options = {})
+        body = { app: app_id, record: {} }
 
-        else
+        # for id
+        body[:id] = options[:id] if options[:id]
 
+        # for  updatekey
+        body[:updateKey] = options[:update_key] if !body[:id] && options[:update_key]
+
+        # for revision
+        body[:revision] = options{:revision} if options[:revision]
+
+        # for fields
+        hash.each do |field_code, value|
+          body[:record][field_code] =
+            if value.is_a?(Array)
+              # if  field type is table, value contains id, field_codes and value in table
+              {}.tap do |table_value|
+                table_value[:value] = value.map do |el|
+                  {}.tap do |hash|
+                    hash[:id] = el[:id]
+                    hash[:value] = {}
+                    el[:value].each do |tf_code, val|
+                      hash[:value][tf_code] = {value: val}
+                    end
+                  end
+                end
+              end
+            else
+              {value: value}
+            end
         end
+
+        body
       end
 
     end # class Record
